14cca1750c75259e5b946083d47c323487a7f9d2
La Barovier <hornosu@yahoo.es>
2024-11-25 13:54:11 +0100

    Little dustbins on modal gallery photos

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -315,17 +315,37 @@ function previousPage () {
 
 //Functions to display the gallery.
 function tinyGalleryDisplay(portfolia) {
+    const svgElement = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/>
+		</svg>`;
+    //svgElement.className = '.dustbinIcon';
+    // Añadir la imagen a un contenedor existente en el DOM
     let modalPortfolio = document.querySelector(".modalGallery");
     portfolia.forEach(item => {
-        let tinyProject = document.createElement("figure");
-        modalPortfolio.appendChild(tinyProject);
-
+        let tinyGallery = document.createElement("figure");
+        modalPortfolio.appendChild(tinyGallery);
         let tinyProjectImage = document.createElement("img");
         tinyProjectImage.src = item.imageUrl;
-        tinyProject.appendChild(tinyProjectImage);
-    });    
+        tinyProjectImage.className = 'modalCanvas';        
+        tinyGallery.appendChild(tinyProjectImage);
+        insertIcon (tinyGallery);
+    }); 
     return;
 }
+
+function insertIcon (container) {
+    const svgElement = `<svg class="dustbinIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+			<path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/>
+		</svg>`;
+    //let canvasModal = document.querySelector(".modalCanvas");
+    /*let delIcon = document.createElement("svg");
+    delIcon.src = svgElement;
+    delIcon.className = 'dustbinIcon';*/
+    container.innerHTML += svgElement;    
+    return;
+} 
+//A ver cómo haces para que esto salga en la moda, porque sale hasta abajo.*/
+
 /*We create a function to empty the tiny gallery any time a button is pressed, 
 so it is brand new when we see the selected photos.*/
 function emptyTinyGallery () {
